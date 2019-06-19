8fc4f6e95a77df49aff01bbf4b8d286dc1c18cc5
alnashi <yamanashi@nashipc.local>
2019-06-19 18:58:22 +0900

    計算諸々

---
+++ git/cul.rb
@@ -0,0 +1,40 @@
+a = a + 1
+a += 1
+name = var || "Ruby"
+
+c = 1
+d = 3
+
+max = (c>d) ? c : d
+p max
+
+p (1..10).to_a
+p (1...10).to_a
+
+class Point
+    attr_accessor :x, :y
+
+    def initialize(x=0, y=0)
+        @x, @y = x,y
+    end
+
+    def inspect
+        "(#{x}, #{y})"
+    end
+
+    def +(other)
+        self.class.new(x + other.x,y + other.y)
+    end
+
+    def -(other)
+        self.class.new(x - other.x,y - other.y)
+    end
+end
+
+point0 =Point.new(3,6)
+point1 = Point.new(1,8)
+
+p point0
+p point1
+p point0 + point1
+p point0 - point1
